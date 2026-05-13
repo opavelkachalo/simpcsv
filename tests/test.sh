2f8e2ca1af2f0b41882aea03fc71a3efcfd4cd74
Pavel Kachalo <opavelkachalo@gmail.com>
2026-05-13 15:00:04 +0300

    add test script and test cases (in/ and out/ dirs)

---
+++ tests/test.sh
@@ -0,0 +1,41 @@
+#/bin/sh
+
+flag="f"
+if [ $# -ge 1 ]; then
+	flag=$1
+fi
+
+n_succ=0
+n_fail=0
+for csv in $(ls in/); do
+	../simpcsv in/$csv > tmp
+	if ! diff tmp out/$csv.out &>/dev/null; then
+		n_fail=$((n_fail + 1))
+		if [ $flag = "f" -o $flag = "sf" ]; then
+			echo ========= Test failed =========
+			echo [INPUT] \(in/$csv\):
+			cat in/$csv
+			echo -e "\n[EXPECTED]" \(out/$csv.out\):
+			cat out/$csv.out
+			echo -e "\n[GOT]:"
+			cat tmp
+		fi
+	else
+		n_succ=$((n_succ + 1))
+		if [ $flag = "s" -o $flag = "sf" ]; then
+			echo ========= Test succeeded =========
+			echo [INPUT] \(in/$csv\):
+			cat in/$csv
+			echo -e "\n[GOT]:"
+			cat tmp
+		fi
+	fi
+done
+
+echo ========= Results =========
+echo passed $n_succ tests
+echo failed $n_fail tests
+
+if [ -e tmp ]; then
+	rm tmp
+fi
